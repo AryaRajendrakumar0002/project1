8ea8dcd804c25696fe19726c423d5be526ef2ff5
Arya <mailtoaryaraj@gmail.com>
2023-09-19 17:50:04 +0530

    latest

--- test1.js
+++ test1.js
@@ -1,2 +1,50 @@
 console.log("hello world");
 console.log("everything is perfect");
+// console.log("Welcome");
+// let obj={
+//     name:'arya',age:22,id:14,
+//     innerobj:{
+//         place:"palakkad",country:"India"
+//     }
+// }
+// console.log(obj.innerobj);
+//  let arr = [2,4,5];//swap array elements,basic method
+// const temp = arr[0];
+// arr[0] = arr[1];
+// arr[1] = temp;
+// console.log(arr);
+// [arr[1],arr[2]] = [arr[2],arr[1]];//preferrable simple method
+// console.log(arr);
+let fruits = ['apple','mango','orange','grape'];
+// for(let i in fruits){
+//     console.log(fruits[i]);
+
+// }
+// let person={
+//     name:"arya",
+//     age:5
+// }
+// for(let i in person){
+//     console.log(person[i]);
+// }
+// for(let i of fruits){
+//     console.log(i)
+// }
+// 
+// let i = 1;
+// while(i<=5){
+//     console.log(i);
+//     i++;
+// }
+// 
+let i =0;
+do {
+    
+    console.log(fruits[i]);
+    i++
+} while (i<fruits.length);
+
+
+
+
+
